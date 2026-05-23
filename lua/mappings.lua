a32c3c85f86783d66facd6e3727fd953fcbf7d06
quantdream <priolo.giuseppe01@proton.me>
2026-05-23 07:50:17 +0200

    Added github support

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -37,3 +37,50 @@ map("n", "<leader>cd", "<cmd>Copilot disable<CR>", {
 map("n", "<leader>cs", "<cmd>Copilot status<CR>", {
   desc = "Copilot Status",
 })
+
+-- Git / Gitsigns
+map("n", "]c", function()
+  if vim.wo.diff then
+    vim.cmd.normal { "]c", bang = true }
+  else
+    require("gitsigns").nav_hunk "next"
+  end
+end, { desc = "Next Git hunk" })
+
+map("n", "[c", function()
+  if vim.wo.diff then
+    vim.cmd.normal { "[c", bang = true }
+  else
+    require("gitsigns").nav_hunk "prev"
+  end
+end, { desc = "Previous Git hunk" })
+
+map("n", "<leader>gs", "<cmd>Gitsigns stage_hunk<CR>", { desc = "Git stage hunk" })
+map("n", "<leader>gr", "<cmd>Gitsigns reset_hunk<CR>", { desc = "Git reset hunk" })
+map("v", "<leader>gs", ":Gitsigns stage_hunk<CR>", { desc = "Git stage selected hunk" })
+map("v", "<leader>gr", ":Gitsigns reset_hunk<CR>", { desc = "Git reset selected hunk" })
+
+map("n", "<leader>gS", "<cmd>Gitsigns stage_buffer<CR>", { desc = "Git stage buffer" })
+map("n", "<leader>gR", "<cmd>Gitsigns reset_buffer<CR>", { desc = "Git reset buffer" })
+
+map("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", { desc = "Git preview hunk" })
+map("n", "<leader>gi", "<cmd>Gitsigns preview_hunk_inline<CR>", { desc = "Git preview hunk inline" })
+
+map("n", "<leader>gb", "<cmd>Gitsigns blame_line full=true<CR>", { desc = "Git blame line" })
+map("n", "<leader>gd", "<cmd>Gitsigns diffthis<CR>", { desc = "Git diff this" })
+map("n", "<leader>gD", function()
+  require("gitsigns").diffthis "~"
+end, { desc = "Git diff against previous" })
+
+map("n", "<leader>gq", "<cmd>Gitsigns setqflist<CR>", { desc = "Git hunks to quickfix" })
+map("n", "<leader>gQ", function()
+  require("gitsigns").setqflist "all"
+end, { desc = "Git all hunks to quickfix" })
+
+map("n", "<leader>gtb", "<cmd>Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle Git line blame" })
+map("n", "<leader>gtw", "<cmd>Gitsigns toggle_word_diff<CR>", { desc = "Toggle Git word diff" })
+
+map({ "o", "x" }, "ih", "<cmd>Gitsigns select_hunk<CR>", { desc = "Git select hunk" })
+
+-- Lazygit
+map("n", "<leader>gg", "<cmd>LazyGit<CR>", { desc = "Open LazyGit" })
